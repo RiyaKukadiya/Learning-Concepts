244c865c1a3c69acbe65a4ac994ffcf5ad0e33fc
RiyaKukadiya <kukadiyariya.7404@gmail.com>
2025-06-13 15:58:20 +0530

    change the header menus.

--- learning-concepts/src/app/components/Header.tsx
+++ learning-concepts/src/app/components/Header.tsx
@@ -3,14 +3,24 @@ import Link from "next/link";
 
 const Header = () => (
   <header className="bg-gray-900 text-white py-4 px-6 flex justify-between items-center">
-    <div className="text-xl font-bold">Learning Concepts</div>
+    <div>
+      <Link href="/" className="text-xl font-bold">
+        Learning
+      </Link>
+    </div>
     <nav>
       <ul className="flex space-x-6">
         <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
           <Link href="/http-status-code">HTTP Status Code</Link>
+        </li>
+         <li>
+          <Link href="/http-status-code">React Hooks</Link>
+        </li>
+         <li>
+          <Link href="/http-status-code">DNS System</Link>
+        </li>
+         <li>
+          <Link href="/http-status-code">ES6</Link>
         </li>
       </ul>
     </nav>
