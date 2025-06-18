4dd1ffccdd2ccec834839bd745a691d4a15a741e
RiyaKukadiya <kukadiyariya.7404@gmail.com>
2025-06-18 12:00:27 +0530

    add animationin the svg example.

--- learning-concepts/src/app/page.tsx
+++ learning-concepts/src/app/page.tsx
@@ -5,8 +5,11 @@ const app = () => {
   return (
     <main className="flex flex-col items-center min-h-screen bg-gray-50">
       <CardList />
-      <div className="w-full flex justify-center">
+      <div className="w-full flex flex-row justify-center">
         <DraggableList items={["React", "Vue", "Angular", "Svelte", "Solid"]} />
+      </div>
+      <div className="w-2/3 flex justify-center mt-4">
+        <img src="/Marketecture Diagram1_option A (2) (1).svg" />
       </div>
     </main>
   );
