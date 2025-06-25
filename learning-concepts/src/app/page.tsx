b5e48d83dc28afe8def90c2b31ffb64b7789085b
RiyaKukadiya <kukadiyariya.7404@gmail.com>
2025-06-25 10:28:22 +0530

    add the object-fit css property in homepage.

--- learning-concepts/src/app/page.tsx
+++ learning-concepts/src/app/page.tsx
@@ -232,14 +232,10 @@ const app = () => {
           </motion.g>
         </motion.svg>
       </div>
-      {/* <div className="border-8 border-black flex items-center justify-center m-5 w-96">
-          <img src="/sunset.jpg" className="" />
-        </div>
-        // Change object-* here: object-contain, object-fill, object-none, object-scale-down */}
       <div className="grid grid-cols-3 text-center">
         <div>
           <h1> With Object-cover </h1>
-          <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
+          <div className="border-8 border-black flex items-center justify-center m-5 w-72 h-64">
             <img
               src="/sunset.jpg"
               className="w-60 h-60 object-cover"
@@ -248,38 +244,87 @@ const app = () => {
           </div>
         </div>
         <div>
-        <h1> With Object-contain </h1>
-        <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
-          <img
-            src="/sunset.jpg"
-            className="w-60 h-60 object-contain"
-            alt="Sunset"
-          />
-        </div>
-        </div>
-        <div>
-        <h1> With Object-fill </h1>
-        <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
-          <img
-            src="/sunset.jpg"
-            className="w-60 h-60 object-fill"
-            alt="Sunset"
-          />
-        </div>
+          <h1> With Object-contain </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-72 h-64">
+            <img
+              src="/sunset.jpg"
+              className="w-60 h-60 object-contain"
+              alt="Sunset"
+            />
+          </div>
         </div>
         <div>
-        <h1> With Object-scale-down </h1>
-        <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
-          <img src="/sunset.jpg" className="w-60 h-60 object-scale-down" alt="Sunset" />
-        </div>
+          <h1> With Object-fill </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-72 h-64">
+            <img
+              src="/sunset.jpg"
+              className="w-60 h-60 object-fill"
+              alt="Sunset"
+            />
+          </div>
         </div>
         <div>
-        <h1> Without object property </h1>
-        <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
-          <img src="/sunset.jpg" className="w-60 h-60" alt="Sunset" />
+          <h1> With Object-scale-down </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-72 h-64">
+            <img
+              src="/sunset.jpg"
+              className="w-60 h-60 object-scale-down"
+              alt="Sunset"
+            />
+          </div>
         </div>
+        <div>
+          <h1> Without object property </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-72 h-64">
+            <img src="/sunset.jpg" className="w-60 h-60" alt="Sunset" />
+          </div>
         </div>
       </div>
+
+      {/* <div className="grid grid-cols-3 text-center">
+        <div>
+          <h1> With Object-cover </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
+            <img src="/sunset.jpg" className="object-cover" alt="Sunset" />
+          </div>
+        </div>
+        <div>
+          <h1> With Object-contain </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
+            <img
+              src="/sunset.jpg"
+              className="w-full h-full object-contain"
+              alt="Sunset"
+            />
+          </div>
+        </div>
+        <div>
+          <h1> With Object-fill </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
+            <img
+              src="/sunset.jpg"
+              className="w-full h-full object-fill"
+              alt="Sunset"
+            />
+          </div>
+        </div>
+        <div>
+          <h1> With Object-scale-down </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
+            <img
+              src="/sunset.jpg"
+              className="w-full h-full object-scale-down"
+              alt="Sunset"
+            />
+          </div>
+        </div>
+        <div>
+          <h1> Without object property </h1>
+          <div className="border-8 border-black flex items-center justify-center m-5 w-96 h-64">
+            <img src="/sunset.jpg" className="w-full h-full" alt="Sunset" />
+          </div>
+        </div>
+      </div> */}
       <div className="w-2/3 flex justify-center mt-4">
         <img src="/Marketecture Diagram1_option A (2) (1).svg" />
       </div>
